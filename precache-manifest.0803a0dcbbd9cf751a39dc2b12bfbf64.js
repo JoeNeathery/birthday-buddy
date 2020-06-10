7d4123465e1c71fe6edc52e7d0aacde6f5b09550
Joe Neeathery <jneathery1201@gmail.com>
2020-06-10 01:02:49 -0400

    Updates

---
+++ precache-manifest.0803a0dcbbd9cf751a39dc2b12bfbf64.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82f873e10e439afec77336b046851d62",
+    "url": "/birthday-buddy/index.html"
+  },
+  {
+    "revision": "832a550ed962c637a7d9",
+    "url": "/birthday-buddy/static/css/2.a1b7a17e.chunk.css"
+  },
+  {
+    "revision": "85e2bbd167d25743b04a",
+    "url": "/birthday-buddy/static/css/main.825a1045.chunk.css"
+  },
+  {
+    "revision": "832a550ed962c637a7d9",
+    "url": "/birthday-buddy/static/js/2.7402df6c.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/birthday-buddy/static/js/2.7402df6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85e2bbd167d25743b04a",
+    "url": "/birthday-buddy/static/js/main.919f0a19.chunk.js"
+  },
+  {
+    "revision": "8b68bc7db82cbf1b5772",
+    "url": "/birthday-buddy/static/js/runtime-main.f09926a1.js"
+  },
+  {
+    "revision": "dfa03c586e2ee0e9e4f7291b42c69fcd",
+    "url": "/birthday-buddy/static/media/login.dfa03c58.svg"
+  }
+]);
